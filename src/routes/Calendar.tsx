6917cb802f2ba7add5ebf02c5571152606dc0a0a
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2023-02-28 13:16:57 +0000

    [WIP] Calendar implementation

--- src/routes/Calendar.tsx
+++ src/routes/Calendar.tsx
@@ -1,35 +1,62 @@
-import { Box, Divider, Heading } from "@chakra-ui/react";
-import FullCalendar, { EventClickArg, EventSourceInput } from "@fullcalendar/react";
+import { Box, Divider, Heading, HStack, Square, Tag, Text } from "@chakra-ui/react";
+import FullCalendar, { EventClickArg, EventApi, EventInput } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import timegridPlugin from "@fullcalendar/timegrid";
+import { EventSourceInput } from "@fullcalendar/react";
 import "../styles/main.css";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { client } from "../utils/api/client";
 import { useNavigate } from "react-router-dom";
+import { components } from "../schema/main";
 
-const Calendar = (): JSX.Element => {
-  const [events, setEvents] = useState<EventSourceInput>({});
+type SessionSchema = components["schemas"]["SessionResponse"];
+
+interface EventProps {
+  info: EventApi;
+}
+
+const EventItem = ({ info }: EventProps) => {
+  return (
+    <Box cursor='pointer' w='100%'>
+      <HStack textOverflow='ellipsis' spacing={1} width='100%'>
+        <Text fontWeight={600} color='diamond.600'>
+          {info.start?.toLocaleTimeString("en-gb", { hour: "2-digit", minute: "2-digit" })}
+        </Text>
+        <Text>{info.title}</Text>
+        <Text textOverflow='ellipsis' overflowX='hidden' opacity='0.7'>
+          ({info.extendedProps.parentProposal}-{info.extendedProps.visitNumber})
+        </Text>
+      </HStack>
+      <Divider />
+    </Box>
+  );
+};
+
+const Calendar = () => {
   const navigate = useNavigate();
 
   const eventClick = (e: EventClickArg) => {
     navigate(`/proposals/${e.event.extendedProps.proposalId}/sessions/${e.event.id}`);
   };
 
-  useEffect(() => {
-    document.title = "PATo » Calendar";
-    client.safe_get("sessions?minDate=2022-10-01&maxDate=2022-10-31").then((response) => {
-      const events = response.data.map((event: Record<string, any>) => ({
-        id: event.sessionId,
-        start: event.startDate,
-        end: event.endDate,
-        title: event.beamLineName,
-        extendedProps: {
-          proposalId: event.proposalId,
-        },
-      }));
-
-      setEvents(events);
-    });
+  const getEvents = useCallback((info: any , success: (e: EventInput[]) => void) => {
+    console.log(info);
+    client
+      .safe_get(
+        `sessions?minStartDate=${info.start.toISOString()}&maxStartDate=${info.end.toISOString()}&search=m&limit=250`
+      )
+      .then((response) => {
+        const events: EventInput[] = response.data.items.map((event: SessionSchema) => ({
+          id: event.sessionId,
+          start: event.startDate,
+          title: event.beamLineName,
+          extendedProps: {
+            proposalId: event.proposalId,
+            parentProposal: event.parentProposal,
+            visitNumber: event.visit_number,
+          },
+        }));
+        success(events);
+      });
   }, []);
 
   return (
@@ -38,19 +65,16 @@ const Calendar = (): JSX.Element => {
       <Divider />
       <Box paddingTop={3}>
         <FullCalendar
-          height='70vh'
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,timeGridWeek,timeGridDay",
-          }}
-          plugins={[dayGridPlugin, timegridPlugin]}
-          events={events}
+          height='80vh'
+          plugins={[dayGridPlugin]}
+          //events={(info, success) => getEvents(info, success)}
           eventTimeFormat={{
             hour: "2-digit",
             minute: "2-digit",
             meridiem: false,
           }}
+          datesSet={(info) => {console.log(info)}}
+          eventContent={(info) => <EventItem info={info.event} />}
           dayMaxEventRows={true}
           dayMaxEvents={true}
           eventClick={(e) => eventClick(e)}
