e11dbcdac47e5292ebc32e3a00dc4622cab34a34
Guilherme Francisco <guilherme.de-freitas@diamond.ac.uk>
2023-04-04 16:29:39 +0100

    Update jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -6,9 +6,7 @@ import "whatwg-fetch";
 process.env.REACT_APP_API_ENDPOINT = "http://localhost/";
 
 beforeEach(() => server.listen());
-afterEach(() => {
-  server.resetHandlers();
-});
+afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 class ResizeObserver {
@@ -22,6 +20,7 @@ global.window.scrollTo = () => {};
 global.structuredClone = (val: Record<string, any>) => JSON.parse(JSON.stringify(val));
 global.URL.createObjectURL = (url: Blob | MediaSource) => "blob://test";
 
+jest.mock("./src/store/store");
 jest.mock("molstar/lib/mol-canvas3d/canvas3d");
 jest.mock("molstar/lib/mol-plugin/context");
 jest.mock("molstar/lib/mol-plugin/spec");
