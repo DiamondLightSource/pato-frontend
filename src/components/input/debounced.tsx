fa33e0bdfd8adb9f39ac5989c764b7451e339161
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2023-04-14 16:43:38 +0100

    Removes Lodash debounce dependency

--- src/components/input/debounced.tsx
+++ src/components/input/debounced.tsx
@@ -1,15 +1,22 @@
 import { Input, InputProps } from "@chakra-ui/react";
 import { FormEvent } from "react";
-import { debounce } from "lodash";
 
 interface DebounceProps extends InputProps {
   onChangeEnd: (text: string) => void;
 }
 
+const debounce = (fn: Function, ms = 300) => {
+  let timer: NodeJS.Timeout;
+
+  return (...args: any[]) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), ms);
+  };
+};
+
 const DebouncedInput = ({ onChangeEnd, ...props }: DebounceProps) => {
-  const handleUpdate = debounce(
-    (event: FormEvent<HTMLInputElement>) => onChangeEnd((event.target as HTMLInputElement).value),
-    1000
+  const handleUpdate = debounce((event: FormEvent<HTMLInputElement>) =>
+    onChangeEnd((event.target as HTMLInputElement).value)
   );
 
   return (
