791cd64c748cd4f6a84b0c6a6c37b02fac3d4dee
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2026-05-13 14:51:16 +0100

    Add soft x-ray redirects

--- src/loaders/session.ts
+++ src/loaders/session.ts
@@ -86,6 +86,7 @@ export const handleGroupClicked = (item: Record<string, string | number>) => {
   switch (item.experimentTypeName) {
     case "Single Particle":
       return `groups/${item.dataCollectionGroupId}/spa`;
+    case "Soft X-Ray Tomography":
     case "Tomography":
       return `groups/${item.dataCollectionGroupId}/tomograms/1`;
     case "CLEM":
